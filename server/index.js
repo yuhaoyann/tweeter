cacf8bdce75dd992931e4b44975aa14a6656e22f
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-12 15:37:10 -0400

    installed packages and changed body-parser to express-encoded

--- server/index.js
+++ server/index.js
@@ -4,10 +4,9 @@
 
 const PORT          = 8080;
 const express       = require("express");
-const bodyParser    = require("body-parser");
 const app           = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
