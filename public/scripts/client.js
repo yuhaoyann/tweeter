db283dd8e1a0bae732ab25fc87d8212e3fdd662a
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-19 01:32:46 -0400

    automaticlly append tweets to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,43 @@
 $(document).ready(function() {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const createTweetElement = function(tweetData) {
+const createTweetElement = function(tweet) {
   const $tweet = `
   <article>
           <header>
             <div class="tweet-header-left">
-              <img src=${tweetData.user.avatars}>
-              <p class="tweet-header-name">${tweetData.user.name}</p>
+              <img src=${tweet.user.avatars}>
+              <p class="tweet-header-name">${tweet.user.name}</p>
             </div>
-            <p class="tweet-header-handle">${tweetData.user.handle}</p>
+            <p class="tweet-header-handle">${tweet.user.handle}</p>
           </header>
-          <p class="tweet-article-content">${tweetData.content.text}</p>
+          <p class="tweet-article-content">${tweet.content.text}</p>
           <footer>
-            <div class="tweet-footer-time">${tweetData.created_at}</div>
+            <div class="tweet-footer-time">${timeago.format(tweet.created_at)}</div>
             <div class="tweet-footer-logo">
               <i class="fas fa-flag"></i>
               <i class="fas fa-retweet"></i>
@@ -36,9 +49,14 @@ const createTweetElement = function(tweetData) {
   return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweet-article").append($tweet);
+  }
+}
 
-$('.tweet-article').append($tweet);
+renderTweets(data);
 
 })
 
