58c62dba7558f33a10be8ce2bf23ea9d2f19c0e8
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-19 16:21:51 -0400

    added function to show existing and new tweets to the page without refreshing the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
 const createTweetElement = function(tweet) {
   const $tweet = `
   <article>
@@ -50,15 +25,28 @@ const createTweetElement = function(tweet) {
 }
 
 const renderTweets = function (tweets) {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
+  for (let i = tweets.length - 1; i >= 0; i--) {
+    const $tweet = createTweetElement(tweets[i]);
     $(".tweet-article").append($tweet);
   }
 }
 
-renderTweets(data);
+$.get('/tweets', function(data) {
+
+  renderTweets(data);
+
+  $(".tweet-article article").hover(function() {
+    $(this).css("box-shadow", "5px 5px rgb(142, 164, 172)");
+  }, function() {
+    $(this).css("box-shadow", "none");
+  })
+
+  $(".tweet-article i").hover(function() {
+    $(this).css("color", "rgb(158, 117, 14)");
+  }, function() {
+    $(this).css("color", "rgb(45, 19, 68)");
+  })
 
 })
 
-
-
+})
--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -2,6 +2,70 @@ $(document).ready(function() {
   $(".new-tweet form").submit(function(event) {
     event.preventDefault();
     let data = $( this ).serialize();
-    $.post("/tweets/", data)
+    // $.post("/tweets/", data);
+  $.post( "/tweets/", data, function() {
+    })
+      .done(function() {
+        newPost();
+      })
+      .fail(function(xhr, status, error) {
+        alert(1+xhr +2+ status +3+ error)
+      })
+      .always(function() {
+        $("#tweet-text").val('')
+        $(".counter").val(140)
+
+      });
+
+
+    function newPost() {
+      const createTweetElement = function(tweet) {
+        const $tweet = `
+        <article>
+                <header>
+                  <div class="tweet-header-left">
+                    <img src=${tweet.user.avatars}>
+                    <p class="tweet-header-name">${tweet.user.name}</p>
+                  </div>
+                  <p class="tweet-header-handle">${tweet.user.handle}</p>
+                </header>
+                <p class="tweet-article-content">${tweet.content.text}</p>
+                <footer>
+                  <div class="tweet-footer-time">${timeago.format(tweet.created_at)}</div>
+                  <div class="tweet-footer-logo">
+                    <i class="fas fa-flag"></i>
+                    <i class="fas fa-retweet"></i>
+                    <i class="fas fa-heart"></i>
+                  </div>
+                </footer>
+              </article>
+              `
+        return $tweet;
+      }
+      
+      const renderTweets = function (tweets) {
+          const $tweet = createTweetElement(tweets[tweets.length - 1]);
+          $(".tweet-article").prepend($tweet);
+      }
+      
+      $.get('/tweets', function(data) {
+      
+        setTimeout(renderTweets(data), 10000)
+        
+      
+        $(".tweet-article article").hover(function() {
+          $(this).css("box-shadow", "5px 5px rgb(142, 164, 172)");
+        }, function() {
+          $(this).css("box-shadow", "none");
+        })
+      
+        $(".tweet-article i").hover(function() {
+          $(this).css("color", "rgb(158, 117, 14)");
+        }, function() {
+          $(this).css("color", "rgb(45, 19, 68)");
+        })
+      
+      })
+    }
   })
 })
