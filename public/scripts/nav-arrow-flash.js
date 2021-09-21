dfae65f4ae5cab1b5f546e9a583d6fdc2d7f99fa
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-21 02:43:55 -0400

    format to make nav top-right arrow to move up and down

--- public/scripts/nav-arrow-flash.js
+++ public/scripts/nav-arrow-flash.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   function foo() {
     setTimeout(foo1, 500);
-    $("#nav-arrow").css("color", "white");
+    $("#nav-arrow").css("padding-top", "5px");
   }
   function foo1() {
-    setTimeout(foo, 500);
-    $("#nav-arrow").css("color", "red");
+    setTimeout(foo2, 500);
+    $("#nav-arrow").css("padding-top", "10px");
   }
-  foo();  
+  function foo2() {
+    setTimeout(foo3, 500);
+    $("#nav-arrow").css("padding-top", "15px");
+  }
+  function foo3() {
+    setTimeout(foo, 500);
+    $("#nav-arrow").css("padding-top", "10px");
+  }
+  foo();
 })
